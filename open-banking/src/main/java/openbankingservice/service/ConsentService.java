1ab44cc489766ef35f45125a97c46b1cd2068c8b
Siarhei Liaukovich <liaukovich_si@st.by>
2021-08-06 14:29:26 +0300

    Изменена работа с согласием. в момент сохранения рандомно привязывается клиент. рандомно генерятся 2 сценария статуса согласия при сохранении.

--- open-banking/src/main/java/openbankingservice/service/ConsentService.java
+++ open-banking/src/main/java/openbankingservice/service/ConsentService.java
@@ -1,5 +1,7 @@
 package openbankingservice.service;
 
+import lombok.RequiredArgsConstructor;
+import openbankingservice.data.entity.AccountEntity;
 import openbankingservice.data.entity.ClientEntity;
 import openbankingservice.data.entity.ConsentEntity;
 import openbankingservice.data.repository.ConsentRepository;
@@ -13,57 +15,82 @@ import openbankingservice.util.ConsentConverter;
 import openbankingservice.util.OBHttpHeaders;
 import openbankingservice.util.StubData;
 import openbankingservice.validation.ConsentBody;
-import lombok.RequiredArgsConstructor;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.junit.Before;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionTemplate;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.hibernate.Transaction;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.validation.Valid;
 import javax.validation.constraints.Size;
-import java.util.Date;
-import java.util.Random;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 @Validated
+
 public class ConsentService {
 
     private final ConsentRepository mConsentRepository;
-
     private final FintechService mFintechService;
     private final ClientService mClientService;
 
-    @Transactional
+        @Transactional
     public ResponseEntity<ConsentResponse> createConsent(
             @Valid @ConsentBody final Consent body,
             final String xFapiAuthDate,
             final String xFapiCustomerIpAddress,
             final String xFapiInteractionId,
-            final String authorization,
-            final String xApiKey
+            @RequestHeader(value = "authorization", required = false)  String authorization,
+            @RequestHeader(value = "x-api-key", required = false)  String xApiKey
     ) {
+
         final HttpHeaders headers = new HttpHeaders();
         headers.add(OBHttpHeaders.X_FAPI_INTERACTION_ID, xFapiInteractionId);
+        headers.add(OBHttpHeaders.AUTHORIZATION,authorization);
+
 
         final Date now = new Date();
 
         final ConsentEntity consent = ConsentConverter.toConsentEntity(body.getData());
-        consent.setStatus(AccountConsentsStatus.AWAITINGAUTHORISATION);
+        //рандомное авторизация или отклонение согласия для демо
+        List<AccountConsentsStatus> accountConsentsStatusList = new ArrayList<>();
+        accountConsentsStatusList.add(AccountConsentsStatus.AUTHORISED);
+        accountConsentsStatusList.add(AccountConsentsStatus.REJECTED);
+        consent.setStatus(accountConsentsStatusList.get(new Random().nextInt(accountConsentsStatusList.size())));
+
+        //получаем рандомного клиента для привязки согласия
+        Object[] values = StubData.CLIENTS.keySet().toArray();
+        String randomValue =(String) values[new Random().nextInt(values.length)];
+        final ClientEntity client = mClientService.identifyClient(randomValue);
+        consent.setClient(client);
+
         consent.setStatusUpdateTime(now);
         consent.setCreationTime(now);
-        consent.setFintech(mFintechService.identifyFintech(xApiKey));
-
+        consent.setFintech(mFintechService.identifyFintech(authorization));
         mConsentRepository.save(consent);
+        //нужен коммитт транзакции где мы сохраняем согласие, чтобы привязались счета к согласию. красивого решения нету, кроме костылей.
+        consent.getAccounts().addAll(client.getAccounts().stream().filter(account -> new Random().nextBoolean()).collect(Collectors.toList()));
 
-        final ConsentResponse responseContent = new ConsentResponse()
-                .data(ConsentConverter.toConsentResponseData(consent));
+            final ConsentResponse responseContent = new ConsentResponse()
+                .data(ConsentConverter.toConsentResponseDataForPost(consent));
 
         return new ResponseEntity<>(responseContent, headers, HttpStatus.CREATED);
-    }
+
+        }
 
     @Transactional
     public ResponseEntity<Void> authorizeConsent(
@@ -77,22 +104,14 @@ public class ConsentService {
         final ClientEntity client = mClientService.identifyClient(xApiKey);
         final ConsentEntity consent = getConsent(Long.valueOf(xAccountConsentId));
 
-        if (consent.getStatus() == AccountConsentsStatus.AWAITINGAUTHORISATION) {
-
             consent.setClient(client);
-            consent.setStatus(AccountConsentsStatus.AUTHORISED);
             consent.setStatusUpdateTime(new Date());
             consent.getAccounts().addAll(client.getAccounts().stream().filter(account -> new Random().nextBoolean()).collect(Collectors.toList()));
             mConsentRepository.save(consent);
 
-            final HttpHeaders headers = new HttpHeaders();
-            headers.add(OBHttpHeaders.X_FAPI_INTERACTION_ID, xFapiInteractionId);
-
-            return new ResponseEntity<>(headers, HttpStatus.OK);
-        } else {
-            throw new OBException(OBErrorCode.BY_NBRB_RESOURCE_INVALID_CONSENT_STATUS, "Invalid consent status");
+            return new ResponseEntity<>(HttpStatus.OK);
         }
-    }
+
 
     @Transactional
     public ResponseEntity<Void> rejectConsent(
@@ -162,8 +181,8 @@ public class ConsentService {
         mClientService.identifyClient(xApiKey);
         final ConsentEntity consent = getConsent(Long.valueOf(accountConsentId));
 
-        final ConsentResponse consentResponse = new ConsentResponse()
-                .data(ConsentConverter.toConsentResponseData(consent));
+        final ConsentResponse consentResponse = new ConsentResponse();
+                consentResponse.data(ConsentConverter.toConsentResponseDataForGet(consent));
 
         final HttpHeaders headers = new HttpHeaders();
         headers.add(OBHttpHeaders.X_FAPI_INTERACTION_ID, xFapiInteractionId);
@@ -177,7 +196,7 @@ public class ConsentService {
             final String api
     ) {
         final ConsentEntity consent = getConsent(consentId);
-        if (isHavePermission(consent, api)) {
+        if (isHavePermission(consent, api) ) {
             return consent;
         } else {
             throw new RuntimeException("Forbidden");
@@ -189,7 +208,8 @@ public class ConsentService {
             final String api
     ) {
         for (Permission permission : consent.getPermission()) {
-            if (StubData.PERMISSIONS_API.get(permission).contains(api)) {
+            if (StubData.PERMISSIONS_API.get(permission).contains(api)
+                    && consent.getStatus().toString().equals("Authorised")) {
                 return true;
             }
         }
--- open-banking/src/main/java/openbankingservice/util/ConsentConverter.java
+++ open-banking/src/main/java/openbankingservice/util/ConsentConverter.java
@@ -1,6 +1,7 @@
 package openbankingservice.util;
 
 import openbankingservice.data.entity.ConsentEntity;
+import openbankingservice.models.accinfo.AccountConsentsStatus;
 import openbankingservice.models.accinfo.ConsentData;
 import openbankingservice.models.accinfo.ConsentResponseData;
 import openbankingservice.models.accinfo.Permission;
@@ -12,8 +13,21 @@ public final class ConsentConverter {
 
     private ConsentConverter() {
     }
+    //Заглушка для сценария когда после создания согласия, мы должны отобразить статус AwaitingAuthorization, но в базу сохраняем рандомный сценарий.
+    public static ConsentResponseData toConsentResponseDataForPost(final ConsentEntity consentEntity) {
+        return new ConsentResponseData()
+                .link("https://sdbo_business.bank.by/accountConsentsId/" + consentEntity.getId() + "/")
+                .accountConsentId(String.valueOf(consentEntity.getId()))
+                .status(AccountConsentsStatus.AWAITINGAUTHORISATION)
+                .creationDateTime(consentEntity.getCreationTime())
+                .statusUpdateDateTime(consentEntity.getStatusUpdateTime())
+                .expirationDate(consentEntity.getExpirationDate())
+                .transactionFromDate(consentEntity.getTransactionFromDate())
+                .transactionToDate(consentEntity.getTransactionToDate())
+                .permissions(new ArrayList<>(consentEntity.getPermission()));
+    }
 
-    public static ConsentResponseData toConsentResponseData(final ConsentEntity consentEntity) {
+    public static ConsentResponseData toConsentResponseDataForGet(final ConsentEntity consentEntity) {
         return new ConsentResponseData()
                 .link("https://sdbo_business.bank.by/accountConsentsId/" + consentEntity.getId() + "/")
                 .accountConsentId(String.valueOf(consentEntity.getId()))
