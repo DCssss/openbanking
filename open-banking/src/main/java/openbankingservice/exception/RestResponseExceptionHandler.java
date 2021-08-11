42d42c7ab33d997f74eaf9407d9164fa1b2343bc
Габрусёнок Сергей Владимирович <habrusionak_su@st.by>
2021-08-11 16:21:59 +0300

    Поправил httpStatus при ошибке

--- open-banking/src/main/java/openbankingservice/exception/RestResponseExceptionHandler.java
+++ open-banking/src/main/java/openbankingservice/exception/RestResponseExceptionHandler.java
@@ -130,7 +130,7 @@ public final class RestResponseExceptionHandler {
 
         response.getErrors().add(error);
 
-        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(response, httpStatus);
     }
 
     private String createHighLevelCode(final HttpStatus httpStatus) {
